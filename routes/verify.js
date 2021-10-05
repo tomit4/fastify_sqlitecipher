b1d705563426ec623e5c91eca18f6abf39c5b03b
tomit4 <mosssap@gmail.com>
2021-10-04 21:16:25 -0700

    added some notation

--- routes/verify.js
+++ routes/verify.js
@@ -4,11 +4,16 @@ module.exports = async fastify => {
         url: '/verify',
         handler: async (request, reply) => {
             //reply.header('Content-Type', 'application/json')
-            const { passwd } = await request.body
+            const { passwd } = await request.body // possible thanks to fastify-formbody
             if (passwd === process.env.DB_PASS) {
                 const token = await reply.jwtSign({
                     passwd: process.env.DB_PASS,
                 })
+                // Does the job, but cannot redirect after setting cookie, browser hangs on response
+                // if used in conjunction with .redirect()
+
+                // Course of Action:  use reply.sendFile() to send to html page that renders the db
+                // while also having the authentication cookie/jwt in the headers
                 reply
                     .setCookie('token', token, {
                         domain: 'localhost',
